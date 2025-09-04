8a20df4ea31861d991042e6cd70e00680a03fdc1
Guillaume Lagrange <guillaume@glagrange.eu>
2025-09-04 16:36:27 +0200

    chore: make uri nicer to tell appart

---
+++ rust/serialization_deserialization/small.rs
@@ -0,0 +1,93 @@
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+struct TestData {
+    id: u64,
+    name: String,
+    values: Vec<i32>,
+    metadata: Option<String>,
+}
+
+impl TestData {
+    fn sample() -> Self {
+        Self {
+            id: 12345,
+            name: "test_user".to_string(),
+            values: vec![1, 2, 3, 4, 5, 10, 20, 30],
+            metadata: Some("sample metadata".to_string()),
+        }
+    }
+}
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench_group(min_time = Duration::from_secs(3))]
+mod serde_json_benches {
+    use super::*;
+
+    #[divan::bench]
+    fn serialize(bencher: divan::Bencher) {
+        let data = TestData::sample();
+
+        bencher
+            .with_inputs(|| data.clone())
+            .bench_values(|data| serde_json::to_string(&data).unwrap())
+    }
+
+    #[divan::bench]
+    fn deserialize(bencher: divan::Bencher) {
+        let data = TestData::sample();
+        let json = serde_json::to_string(&data).unwrap();
+
+        bencher.with_inputs(|| json.clone()).bench_values(|json| {
+            let _: TestData = serde_json::from_str(&json).unwrap();
+        })
+    }
+
+    #[divan::bench]
+    fn roundtrip_ser_de(bencher: divan::Bencher) {
+        let data = TestData::sample();
+
+        bencher.with_inputs(|| data.clone()).bench_values(|data| {
+            let json = serde_json::to_string(&data).unwrap();
+            let _: TestData = serde_json::from_str(&json).unwrap();
+        })
+    }
+}
+
+#[divan::bench_group(min_time = Duration::from_secs(3))]
+mod sonic_rs_benches {
+    use super::*;
+
+    #[divan::bench]
+    fn serialize(bencher: divan::Bencher) {
+        let data = TestData::sample();
+
+        bencher
+            .with_inputs(|| data.clone())
+            .bench_values(|data| sonic_rs::to_string(&data).unwrap())
+    }
+
+    #[divan::bench]
+    fn deserialize(bencher: divan::Bencher) {
+        let data = TestData::sample();
+        let json = sonic_rs::to_string(&data).unwrap();
+
+        bencher.with_inputs(|| json.clone()).bench_values(|json| {
+            let _: TestData = sonic_rs::from_str(&json).unwrap();
+        })
+    }
+
+    #[divan::bench]
+    fn roundtrip_ser_de(bencher: divan::Bencher) {
+        let data = TestData::sample();
+
+        bencher.with_inputs(|| data.clone()).bench_values(|data| {
+            let json = sonic_rs::to_string(&data).unwrap();
+            let _: TestData = sonic_rs::from_str(&json).unwrap();
+        })
+    }
+}
